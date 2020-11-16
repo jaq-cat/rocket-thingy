5ec97465607b70ff74a0c20a9b1a307e4146f5e5
jaq-cat <jaq.cat@protonmail.ch>
2020-11-16 17:10:59 -0300

    cool

--- src/rocket.hpp
+++ src/rocket.hpp
@@ -10,20 +10,23 @@ static const sf::Color SPACE_ORANGE(sf::Color(250, 145, 5));
 
 class Rocket : public sf::Drawable, public sf::Transformable {
 private:
+    sf::ConvexShape nose;
+    sf::RectangleShape fuselage;
 
 public:
-    Rocket() {}
-    ~Rocket() {}
-    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const {
-        states.transform *= getTransform();
-        sf::ConvexShape nose(3);
+    Rocket()
+    : nose(sf::ConvexShape(3)),
+    fuselage(sf::RectangleShape(sf::Vector2f(SHIP_WIDTH, SHIP_HEIGHT))) {
         nose.setPoint(0, sf::Vector2f(SHIP_WIDTH/2.0, 0));
         nose.setPoint(1, sf::Vector2f(0.0, SHIP_WIDTH));
         nose.setPoint(2, sf::Vector2f(SHIP_WIDTH, SHIP_WIDTH));
-        sf::RectangleShape fuselage(sf::Vector2f(SHIP_WIDTH, SHIP_HEIGHT));
         fuselage.move(sf::Vector2f(0, SHIP_WIDTH));
         nose.setFillColor(SPACE_ORANGE);
         fuselage.setFillColor(sf::Color(0, 0, 0));
+    }
+    ~Rocket() {}
+    virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const {
+        states.transform *= getTransform();
         target.draw(nose);
         target.draw(fuselage);
     }
