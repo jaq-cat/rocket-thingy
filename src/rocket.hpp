a0c02f092fb14df01021ebbb0202b566532aacc3
jaq-cat <jaq.cat@protonmail.ch>
2020-11-16 17:01:58 -0300

    triangle boi

--- src/rocket.hpp
+++ src/rocket.hpp
@@ -1,9 +1,12 @@
 #ifndef ROCKET_HPP
 #define ROCKET_HPP
 #include <SFML/Graphics.hpp>
+#include <cmath>
 
 #define SHIP_HEIGHT 100
 #define SHIP_WIDTH 40
+
+static const sf::Color SPACE_ORANGE(sf::Color(250, 145, 5));
 
 class Rocket : public sf::Drawable, public sf::Transformable {
 private:
@@ -16,7 +19,9 @@ public:
         //sf::CircleShape shape(10);
         sf::CircleShape nose(SHIP_WIDTH, 3);
         sf::RectangleShape fuselage(sf::Vector2f(SHIP_WIDTH, SHIP_HEIGHT));
-        nose.setFillColor(sf::Color(0, 0, 0));
+        fuselage.move(sf::Vector2f(0, (SHIP_WIDTH/2.0) * pow(3.0, 0.5)));
+        nose.setFillColor(SPACE_ORANGE);
+        fuselage.setFillColor(SPACE_ORANGE);
         target.draw(nose);
         target.draw(fuselage);
     }
