57e0d11eaece6fda0782c40c49ca797823f1a13a
jaq-cat <jaq.cat@protonmail.ch>
2020-11-16 16:47:47 -0300

    fuselage

--- src/rocket.hpp
+++ src/rocket.hpp
@@ -1,16 +1,24 @@
 #ifndef ROCKET_HPP
 #define ROCKET_HPP
 #include <SFML/Graphics.hpp>
+
+#define SHIP_HEIGHT 100
+#define SHIP_WIDTH 40
+
 class Rocket : public sf::Drawable, public sf::Transformable {
 private:
+
 public:
     Rocket() {}
     ~Rocket() {}
     virtual void draw(sf::RenderTarget& target, sf::RenderStates states) const {
         states.transform *= getTransform();
-        sf::CircleShape shape(10);
-        shape.setFillColor(sf::Color(255, 0, 255));
-        target.draw(shape);
+        //sf::CircleShape shape(10);
+        sf::CircleShape nose(SHIP_WIDTH, 3);
+        sf::RectangleShape fuselage(sf::Vector2f(SHIP_WIDTH, SHIP_HEIGHT));
+        nose.setFillColor(sf::Color(0, 0, 0));
+        target.draw(nose);
+        target.draw(fuselage);
     }
 
 };
