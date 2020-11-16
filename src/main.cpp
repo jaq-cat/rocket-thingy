a3132b303829c13c6319a9b5a03c1b790b85dc7c
jaq-cat <jaq.cat@protonmail.ch>
2020-11-16 12:59:00 -0300

    boilerplate

---
+++ src/main.cpp
@@ -0,0 +1,66 @@
+#define WIDTH 1400
+#define HEIGHT 1000
+
+#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <unordered_map>
+
+using sf::Vector2u;
+using sf::Vector2i;
+using sf::Vector2f;
+using sf::Color;
+using sf::Event;
+using sf::Keyboard;
+using sf::CircleShape;
+
+using std::string;
+using std::unordered_map;
+
+int main() {
+    // settings
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 4;
+    // create window
+    sf::RenderWindow win(sf::VideoMode(WIDTH, HEIGHT), "Window",
+            sf::Style::Titlebar, settings);
+    win.setPosition(Vector2i(250, 50));
+    //win.setVerticalSyncEnabled(true);
+    win.setFramerateLimit(60);
+    win.setKeyRepeatEnabled(false);
+
+    // input handler
+    unordered_map<char, bool> ih = unordered_map<char, bool>();
+
+    // initialization
+
+    while (win.isOpen()) {
+        // event handling
+        Event ev;
+        while (win.pollEvent(ev)) {
+            switch(ev.type) {
+                case Event::Closed:
+                    win.close();
+                    break;
+                case Event::TextEntered:
+                    break;
+                case Event::KeyPressed:
+                    //if (ev.key.code == Keyboard::W)
+                        //ih['w'] = true;
+                    break;
+                case Event::KeyReleased:
+                    //if (ev.key.code == Keyboard::W)
+                        //ih['w'] = false;
+                    break;
+                default:
+                    break;
+
+            }
+        }
+        // updating
+        // collision
+        // rendering
+        win.clear();
+        win.display();
+    }
+    return 0;
+}
